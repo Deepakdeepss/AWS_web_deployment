f0ac32942d01f5537c8c84594d45e355c50470fd
Deepakdeepss <deepakdeepss123@gmail.com>
2024-12-12 19:35:32 +0530

    Update stop_server.sh 2

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-# Stop the Apache HTTP server
 echo "Stopping Apache HTTP server..."
-sudo systemctl stop httpd
 
-# Disable Apache to prevent it from starting on boot
+# Check if Apache is already stopped
+if systemctl is-active --quiet httpd; then
+  systemctl stop httpd
+  echo "Apache HTTP server stopped."
+else
+  echo "Apache HTTP server is not running."
+fi
+
 echo "Disabling Apache from starting on boot..."
-sudo systemctl disable httpd
+systemctl disable httpd
 
-# Check the status of Apache to confirm it is stopped
-echo "Checking Apache status..."
-sudo systemctl status httpd
+# Check Apache status
+systemctl status httpd
+
 
 
 
